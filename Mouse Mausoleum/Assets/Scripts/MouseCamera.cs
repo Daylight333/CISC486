fea5f8a8952a380222d2392e8ca8665aba7ae510
Dayna Corman <dayna.corman@gmail.com>
2025-09-28 16:30:01 -0400

    Camera Progress

--- Mouse Mausoleum/Assets/Scripts/MouseCamera.cs
+++ Mouse Mausoleum/Assets/Scripts/MouseCamera.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class MouseCamera : MonoBehaviour {
-    public Vector2 turn;
+// public class MouseCamera : MonoBehaviour {
+//     public Vector2 turn;
 
-    void Start(){
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+//     void Start(){
+//         Cursor.lockState = CursorLockMode.Locked;
+//         Cursor.visible = false;
+//     }
 
-    void Update(){
+//     void Update(){
         
-        turn.x += Input.GetAxis("Mouse X");
-        transform.localRotation = Quaternion.Euler(0,turn.x,0);
-    }
-}
+//         turn.x += Input.GetAxis("Mouse X");
+//         transform.localRotation = Quaternion.Euler(0,turn.x,0);
+//     }
+// }
