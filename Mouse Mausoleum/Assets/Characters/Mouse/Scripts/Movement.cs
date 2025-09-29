13174a6b10a78727173e8d98ea904f4f6d43b639
Dayna Corman <dayna.corman@gmail.com>
2025-09-29 15:57:23 -0400

    Camera Fixes
    - Camera doesn't reposition
    - Mouse will move in direction of camera
    - Mouse will rotate in the direction of the camera

--- Mouse Mausoleum/Assets/Characters/Mouse/Scripts/Movement.cs
+++ Mouse Mausoleum/Assets/Characters/Mouse/Scripts/Movement.cs
@@ -4,38 +4,44 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float speed = 2.0f;
     public Rigidbody rb;
-    private float movex = 0f;
-    private float movez = 0f;
-    private Vector3 move = new Vector3(0,0,0);
+    private Vector3 moveDir = new Vector3(0,0,0);
+    private Vector3 viewCamDir;
+    private float horizontal;
+    private float vertical;
+    private Vector3 camForward;
+    private Vector3 camRight;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform virtualCamera;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        movex = 0f;
-        movez = 0f;
-        move = new Vector3(0,0,0);
-        if (Input.GetKey("w")){
-            movez = 1f;
-        }
-        if (Input.GetKey("a")){
-            movex = -1f;
-        }
-        if (Input.GetKey("s")){
-            movez = -1f;
-        }
-        if (Input.GetKey("d")){
-            movex = 1f;
+        // Get the direction the camera is looking
+        viewCamDir = virtualCamera.forward;
+
+        // Flatten it so the character only rotates on the XZ plane
+        viewCamDir.y = 0;
+
+        // Only rotate if direction is valid
+        if (viewCamDir.sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(viewCamDir);
         }
 
-        move = new Vector3(movex, 0f, movez).normalized;
+         // A/D or Left/Right arrows
+        horizontal = Input.GetAxis("Horizontal");
 
-        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        // W/S or Up/Down arrows
+        vertical   = Input.GetAxis("Vertical");
 
+        // Get camera forward and right directions (flattened to XZ plane)
+        camForward = new Vector3(virtualCamera.forward.x, 0, virtualCamera.forward.z).normalized;
+        camRight   = new Vector3(virtualCamera.right.x, 0, virtualCamera.right.z).normalized;
+
+        // Combine input with camera directions
+        moveDir = (camForward * vertical + camRight * horizontal).normalized;
+
+        // Move player
+        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
     }
 }
