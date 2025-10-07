b8e503f97eac38a824ea57bbd16a7dae0207d695
AydanMacGregor <macgregoraydan@gmail.com>
2025-10-07 18:54:01 -0400

    Final Update

--- Mouse Mausoleum/Assets/Characters/Mouse/Scripts/Health.cs
+++ Mouse Mausoleum/Assets/Characters/Mouse/Scripts/Health.cs
@@ -7,7 +7,7 @@ public class Health : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
-        health = 3;
+        health = 2;
     }
 
     // Make sure the game ends when the mouse dies
