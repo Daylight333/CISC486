f63ce6e1298179c14d33e29f981f1b1ba71734ae
Dayna Corman <dayna.corman@gmail.com>
2025-10-20 17:31:34 -0400

    Added mouse health and colour to cheese
    Added health to mouse when picking up cheese and turned cheese yellow upon start all inside the collect script in cheese

--- Mouse Mausoleum/Assets/Characters/Cheese/Collect.cs
+++ Mouse Mausoleum/Assets/Characters/Cheese/Collect.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour {
     
-    public GameObject targetObject;
+    public GameObject mouseObject;
+    public Renderer rend;
+
+    void Start(){
+        rend = GetComponent<MeshRenderer>();
+        rend.material.color = Color.yellow;
+    }
 
     // If the Mouse collides with the cheese make the cheese dissapear
     void OnCollisionEnter(Collision cheeseCollide){
         
-        if (cheeseCollide.gameObject == targetObject){
+        if (cheeseCollide.gameObject == mouseObject){
             gameObject.SetActive(false);
-            Debug.Log("test");
+            
         }
+        mouseObject.GetComponent<Health>().gainHealth();
+        Debug.Log("Cheese collected and one extra health gained");
     }
     
 }
