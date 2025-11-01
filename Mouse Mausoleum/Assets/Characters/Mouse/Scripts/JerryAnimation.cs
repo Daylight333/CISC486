4f41fac578ef837bb2b1221bfb9d8192c9b049f8
AydanMacGregor <macgregoraydan@gmail.com>
2025-11-01 15:38:05 -0400

    Fixing small bugs
    Quick fixing small bugs in the height of the mouse. Adding in a collider for the camera

--- Mouse Mausoleum/Assets/Characters/Mouse/Scripts/JerryAnimation.cs
+++ Mouse Mausoleum/Assets/Characters/Mouse/Scripts/JerryAnimation.cs
@@ -10,6 +10,7 @@ public class Animation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         animator.Play("mixamo_com");
+        transform.position = new Vector3(transformMouse.position.x, (transformMouse.position.y - 1.7f), transformMouse.position.z);
     }
 
     void Update(){
@@ -17,15 +18,15 @@ public class Animation : MonoBehaviour
             Input.GetKey(KeyCode.A) || 
             Input.GetKey(KeyCode.S) || 
             Input.GetKey(KeyCode.D)){
-            if (!hasMoved){
-                transform.position = new Vector3(transformMouse.position.x,0,transformMouse.position.z);
+            if (!hasMoved){//
+                transform.position = new Vector3(transformMouse.position.x, (transformMouse.position.y - 1f), transformMouse.position.z);
                 hasMoved = true;
             }
             animator.Play("JerryRun");
         }
         else {
             if (hasMoved){
-                transform.position = new Vector3(transformMouse.position.x,-moveVal,transformMouse.position.z);
+                transform.position = new Vector3(transformMouse.position.x,(transformMouse.position.y - 1.7f),transformMouse.position.z);
                 hasMoved = false;
             }
             
